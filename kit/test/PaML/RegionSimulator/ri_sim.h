726ac77a0933efedc5f884e6ccfc0fdd5cb31711
Niels Hallenberg <nhallenberg@gmail.com>
2000-01-18 09:48:21 +0000

    Added files for the RegionSimulator

---
+++ kit/test/PaML/RegionSimulator/ri_sim.h
@@ -0,0 +1,60 @@
+#define formID_main_ri_sim       1000
+#define formID_sys_info          1001
+#define formID_card_info         1002
+#define formID_heap_info         1003
+
+#define menuID_ri_sim            1004
+
+#define menuitemID_about         1005
+#define menuitemID_card_info     1006
+
+#define labelID_stack            2000
+#define labelID_stack_addr       2001
+#define labelID_stack_size       2002
+#define labelID_card             2003
+#define labelID_card_no          2004
+#define labelID_card_sel         2005
+#define labelID_card_info_no     2006
+#define labelID_card_name        2007
+#define labelID_manu_name        2008
+#define labelID_version          2009
+#define labelID_cr_date          2010
+#define labelID_ROM_size         2011
+#define labelID_RAM_size         2012
+#define labelID_free_mem         2013
+#define labelID_num_heaps        2014
+#define labelID_num_RAM_heaps    2015
+#define labelID_heap1            2016
+#define labelID_heap1d1          2017
+#define labelID_heap1d2          2018
+#define labelID_heap2            2019
+#define labelID_heap2d1          2020
+#define labelID_heap2d2          2021
+#define labelID_heap3            2022
+#define labelID_heap3d1          2023
+#define labelID_heap3d2          2024
+#define labelID_total_alloc      2025
+#define labelID_num_reg          2026
+#define labelID_stat1            2027
+#define labelID_stat2            2028
+#define labelID_stat3            2029
+
+#define pushbottomID_card1       3000
+#define pushbottomID_card2       3001
+#define pushbottomID_card3       3002
+#define pushbottomID_card4       3003
+#define pushbottomID_card5       3004
+#define pushbottomID_card6       3005
+#define buttonID_show_card_info  3006
+#define buttonID_show_card_no    3007
+#define buttonID_show_heap_info  3008
+#define buttonID_show_heap_no    3009
+#define buttonID_sys_info_done   3010
+
+#define alertID_about            1000
+#define alertID_no_card          1001
+#define alertID_panic            1002
+
+#define helpID_card_info         1000
+#define helpID_heap_info         1001
+
