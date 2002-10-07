4c98b49c520c7d3597b437eb9a72d87eb33bba2c
Kennie Nybo Pontoppidan <knp@rehfeld.dk>
2002-10-07 13:05:45 +0000

    scs-texts module is merged into scs-locales

--- kit/smlserver_demo/scs_lib/orasql/scs-create.sql
+++ kit/smlserver_demo/scs_lib/orasql/scs-create.sql
@@ -35,15 +35,14 @@ show errors
 ------------------------------------
 
 @scs-logs-create.sql;
-@scs-texts-create.sql;
-@scs-enumerations-create.sql;
 @scs-locales-create.sql;
+@scs-enumerations-create.sql;
 @scs-parties-create.sql;
 @scs-roles-create.sql;
 @scs-persons-create.sql;
 @scs-users-create.sql;
 @scs-groups-create.sql;
 
-select table_name from user_tables;
-
+select table_name from user_tables; -- debugging information
+set serveroutput on; -- used by test files
 
--- kit/smlserver_demo/scs_lib/orasql/scs-locales-drop.sql
+++ kit/smlserver_demo/scs_lib/orasql/scs-locales-drop.sql
@@ -1,2 +1,6 @@
+drop package scs_text;
+
 drop table scs_locales;
+drop table scs_text_lang;
 drop table scs_lang;
+drop table scs_texts;
