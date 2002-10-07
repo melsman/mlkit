d3ce66e44fa6fc0d8bbe6712849e52f5746e5d47
Kennie Nybo Pontoppidan <knp@rehfeld.dk>
2002-10-07 13:22:13 +0000

    initial import of test module

---
+++ kit/smlserver_demo/scs_lib/orasql/scs-test-create.sql
@@ -0,0 +1,47 @@
+set serveroutput on; -- used by procedure printTest defined below
+/
+show errors
+
+---------------------------------
+-- scs_test package prototypes --
+---------------------------------
+create or replace package scs_test
+as
+
+  procedure printTest(
+    testname varchar2,
+    expression boolean
+  ) ;
+
+end scs_test;
+/
+show errors
+
+---------------------------
+-- scs_test package body --
+---------------------------
+create or replace package body scs_test
+as
+  procedure printTest(
+    testname varchar2,
+    expression boolean
+  ) 
+  is
+    str varchar(7);
+  begin
+    if expression = TRUE then
+      str := 'true: ';
+    else
+      str := 'false: ';
+    end if;
+    DBMS_output.put_line( str || testname );    
+  end printTest;
+
+end scs_test;
+/ 
+show errors
+
+
+
+
+
