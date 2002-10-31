9b7249350868f298a4aa6f2e39a332846eb563dc
Martin Elsman <mael@diku.dk>
2002-10-31 20:24:36 +0000

    heap caching for SMLserver

---
+++ src/Runtime/HeapCache.c
@@ -0,0 +1,235 @@
+#include "HeapCache.h"
+#include "Region.h"
+#include "Runtime.h"
+
+/*
+ * static function declarations
+ */
+
+// [newHeap()] returns an uninitialized heap - with status 
+// HSTAT_UNINITIALIZED.
+static Heap* newHeap(void);
+
+// [restoreHeap(h)] restores the heap from the heap copy information.
+// Changes the heap status to HSTAT_CLEAN. Requires the heap status to
+// be HSTAT_DIRTY. 
+static void restoreHeap(Heap *h);
+
+// [pagesInRegion(r)] returns the number of pages associated with r.
+static int pagesInRegion(Ro *r);
+
+// [copyRegion(r)] copies the content of the region r into a malloced
+// data structure containing all pages from the region and region
+// descriptor information.
+static RegionCopy* copyRegion(Ro *r);
+
+// [restoreRegion(rc)] restores the region rc->r from the region copy rc
+// by copying back the original region page contents into the first
+// region pages in the region. The function frees the remaining pages
+// of the region. Returns 0 on success and -1 on error.
+static int restoreRegion(RegionCopy *rc);
+
+
+static int heapid_counter = 0;
+
+#ifdef THREADS
+#include "/usr/share/aolserver/include/ns.h"
+extern Ns_Mutex stackPoolMutex;
+#define HEAP_POOL_MUTEX_LOCK     Ns_LockMutex(&stackPoolMutex);
+#define HEAP_POOL_MUTEX_UNLOCK   Ns_UnlockMutex(&stackPoolMutex);
+#else
+#define HEAP_POOL_MUTEX_LOCK
+#define HEAP_POOL_MUTEX_UNLOCK
+#endif
+
+static Heap* heapPool[MAX_HEAP_POOL_SZ];
+static int heapPoolIndex = 0;
+
+// Invariant: if heapPoolIndex == 0 then there are no heaps in the
+// heapPool to choose from; otherwise, the heapPool contains a heap
+// we can use (index heapPoolIndex). Each heap in the pool has status
+// HSTAT_CLEAN.
+
+// [pagesInRegion(r)] returns the number of pages associated with r.
+static int pagesInRegion(Ro *r)
+{
+  Klump *p;
+  int n = 0;
+  for ( p = r->fp ; p ; p = p->n )
+    n++;
+  return n;
+}
+
+static RegionCopy* copyRegion(Ro *r)
+{
+  int np, bytes, *q;
+  Klump *p;
+  RegionCopy *rc;
+
+  if ( r->lobjs ) 
+    {
+      #ifdef THREADS
+      Ns_Log(Error, "Large object constructed during initialization of library code");
+      #endif
+      die ("copyRegion: copying of large objects not supported");
+    }
+  // printf("entering copyRegion r = %x\n", r);
+
+  np = pagesInRegion(r);
+
+  // printf("%d pages\n", np);
+
+  bytes = sizeof(RegionCopy) + 4 /*for final null-pointer*/ 
+    + np * (4 * (ALLOCATABLE_WORDS_IN_REGION_PAGE + 1));
+  rc = (RegionCopy*)malloc(bytes);
+  
+  rc->r = r;     // not really necessary
+  rc->a = r->a;
+  rc->b = r->b;
+
+  q = rc->pages;
+  for ( p = r->fp ; p ; p = p->n )
+    {
+      int i;
+      (Klump*)(*q++) = p;
+      for ( i = 0; i < ALLOCATABLE_WORDS_IN_REGION_PAGE ; i++, q++ )
+	*q = p->i[i];
+    }
+  (Klump*)(*q) = 0;
+  return rc;
+}
+
+static int restoreRegion(RegionCopy *rc)
+{
+  Klump *p;
+  int i = 0;
+  p = (Klump*)(rc->pages[0]);
+
+  while ( (Klump*)(rc->pages[i]) )
+    {
+      int j = 0;
+      p = (Klump*)(rc->pages[i++]);
+      while ( j < ALLOCATABLE_WORDS_IN_REGION_PAGE )
+	p->i[j++] = rc->pages[i++];
+    }
+  free_region_pages(p->n,((Klump*)rc->r->b)-1);
+  p->n = NULL;
+  rc->r->a = rc->a;
+  rc->r->b = rc->b;
+  free_lobjs(rc->r->lobjs);
+  rc->r->lobjs = NULL;
+  return 0;
+}
+
+static Heap* newHeap(void)
+{
+  Heap* h;
+  h = (Heap*)malloc(sizeof(Heap));
+  if ( h == 0 )
+    die ("newHeap: couldn't allocate room for heap");
+  h->status = HSTAT_UNINITIALIZED;
+  h->r0copy = NULL;
+  h->r2copy = NULL;
+  h->r3copy = NULL;
+  h->sp = NULL;
+  return h;
+}
+
+Heap* getHeap(void)
+{
+  Heap* h;
+
+  HEAP_POOL_MUTEX_LOCK;
+  if ( heapPoolIndex )
+    {
+      h = heapPool[--heapPoolIndex];
+      HEAP_POOL_MUTEX_UNLOCK;
+    }
+  else   // allocate new heap
+    { 
+      int hid = heapid_counter++;
+      HEAP_POOL_MUTEX_UNLOCK;
+      h = newHeap();
+      h->heapid = hid;
+    }
+
+#ifdef THREADS
+  //  Ns_Log(Notice, "starting execution in heap %d", h->heapid);
+#endif
+
+  return h;
+}
+
+void touchHeap(Heap* h)
+{
+  if ( h->status != HSTAT_CLEAN )
+    die("touchHeap: status <> HSTAT_CLEAN");
+  h->status = HSTAT_DIRTY;
+}
+
+void deleteHeap(Heap *h)
+{
+  if ( h->r0copy ) free(h->r0copy);
+  if ( h->r2copy ) free(h->r2copy);
+  if ( h->r3copy ) free(h->r3copy);      
+  free(h);
+}
+
+void releaseHeap(Heap *h)
+{
+  restoreHeap(h);
+
+  HEAP_POOL_MUTEX_LOCK;
+  if ( heapPoolIndex <= MAX_HEAP_POOL_SZ ) 
+    {
+      heapPool[heapPoolIndex++] = h;
+      HEAP_POOL_MUTEX_UNLOCK;
+    }
+  else
+    {
+      HEAP_POOL_MUTEX_UNLOCK;
+      deleteHeap(h);
+    } 
+}
+
+static void restoreHeap(Heap *h)
+{
+  if ( h->status != HSTAT_DIRTY )
+    die ("restoreHeap: status <> HSTAT_DIRTY");
+ 
+  if ( restoreRegion(h->r0copy) == -1 )
+    die ("restoreHeap: failed to restore r0");
+
+  if ( restoreRegion(h->r2copy) == -1 )
+    die ("restoreHeap: failed to restore r2");
+
+  if ( restoreRegion(h->r3copy) == -1 )
+    die ("restoreHeap: failed to restore r3");
+
+  h->status = HSTAT_CLEAN;
+}
+
+void initializeHeap(Heap *h, int *sp, int *exnPtr)
+{
+  Ro *r0, *r2, *r3; 
+
+  if ( h->status != HSTAT_UNINITIALIZED )
+    die ("initializeHeap: status <> HSTAT_UNINITIALIZED");
+
+  r0 = clearStatusBits(*(Ro**)(h->ds));
+  r2 = r0+1;
+  r3 = r0+2;
+  
+  h->sp = sp;
+  h->exnPtr = exnPtr;
+
+  //  printf("r0 = %x, r2 = %x, r3=%x, h=%x, ds=%x\n", r0,r2,r3,h,h->ds);
+
+  h->r0copy = copyRegion(r0);
+  h->r2copy = copyRegion(r2);
+  h->r3copy = copyRegion(r3);
+
+  h->status = HSTAT_CLEAN;
+}
+
+
---
+++ src/Runtime/HeapCache.h
@@ -0,0 +1,69 @@
+#ifndef HEAP_CACHE_H
+#define HEAP_CACHE_H
+
+#include "Region.h"
+
+// Pages are layed out in continuous memory, where each page 
+// (ALLOCATABLE_WORDS_IN_REGION_PAGE words) is prefixed with a 
+// pointer to the origin region page.
+
+typedef struct regionCopy {
+  int *a; // allocation pointer
+  int *b; // border pointer
+  Ro *r;  // origin region
+  int pages[0];
+} RegionCopy;
+  
+#define HSTAT_UNINITIALIZED  0
+#define HSTAT_DIRTY          1
+#define HSTAT_CLEAN          2
+
+#define STACK_SIZE_INIT      (20 * 1024 * 1024)
+
+// Maximum number of allocated heaps (stacks and initial region pages)
+// in the heap pool - important only for the multi-threaded SMLserver.
+// The effect of using a heap from the heap pool is that execution of
+// library code is cached. To enable execution of library code for
+// every request, set MAX_HEAP_POOL_SZ to 0.
+#define MAX_HEAP_POOL_SZ     8
+
+typedef struct heap {
+  int heapid;               // unique heap id
+  int status;               // heap status
+  RegionCopy *r0copy;       // rtype top
+  RegionCopy *r2copy;       // rtype pair
+  RegionCopy *r3copy;       // rtype string
+  int *sp;                  // stack pointer
+  int *exnPtr;
+  int ds[STACK_SIZE_INIT];  // start of data-space
+                            //   followed by stack
+} Heap;
+
+// [getHeap()] returns a heap h from the pool of heaps with the status
+// set to either HSTAT_UNINITIALIZED or HSTAT_CLEAN. In the latter
+// case, the stack pointer h->sp and the dataspace counter &(h->ds)
+// can be extracted and used for interpretation; all what remains is
+// to interpret the leaf bytecode. In the former case, library code
+// need first be executed, after which, the initializeHeap() function
+// should be called.
+Heap* getHeap(void);
+
+// [touchHeap(h)] changes the status of the heap h to HSTAT_DIRTY.
+// Requires the status to be HSTAT_CLEAN. 
+void touchHeap(Heap *h);
+
+// [releaseHeap(h)] restores the heap from the heap copy information
+// and gives back the heap h to the pool of heaps. Requires the heap
+// status to be HSTAT_DIRTY.
+void releaseHeap(Heap *h);
+
+// [initializeHeap(h,sp,exnPtr)] This function should be called after
+// library code is executed, but before leaf bytecode is executed. The
+// function changes the status of the heap to HSTAT_CLEAN. It requires
+// the heap status to be HSTAT_UNINITIALIZED. 
+void initializeHeap(Heap *h, int *sp, int *exnPtr);
+
+// [deleteHeap(h)] deletes the heap by freeing it.
+void deleteHeap(Heap *h);
+
+#endif
