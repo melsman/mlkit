26bfb86dd08d13d79e7eac105c74df481c6863ca
Kennie Nybo Pontoppidan <knp@rehfeld.dk>
2002-12-06 13:44:04 +0000

    ready for release

---
+++ smlserver_demo/scs_lib/orasql/scs-enumerations-tables-create.sql
@@ -0,0 +1,64 @@
+-- $Id$
+
+
+create table scs_enumerations(
+  enum_id integer
+    constraint scs_enumerations_enum_id_nn not null
+    constraint scs_enumerations_enum_id_pk primary key,
+  name varchar2(30)
+    constraint scs_enumerations_name_nn not null
+    constraint scs_enumerations_name_un unique
+);
+
+create table scs_enum_values(
+  val_id integer
+    constraint scs_enum_values_val_id_nn not null
+    constraint scs_enum_values_val_id_pk primary key,
+  enum_id integer
+    constraint scs_enum_values_enum_id_nn not null
+    constraint scs_enum_values_enum_id_fk references scs_enumerations(enum_id),
+  text_id integer
+    constraint scs_enum_values_text_id_fk references scs_texts(text_id),
+  value varchar2(100)
+    constraint scs_enum_values_value_nn	not null,
+  active_p char(1) default ('t')
+    constraint scs_enum_values_active_p_nn not null
+    constraint scs_enum_values_active_p_ck
+      check (active_p in ('t','f')),
+  ordering integer
+    constraint scs_enum_values_order_nn not null,
+  constraint scs_enum_values_enu_val_un unique(enum_id,value),
+  constraint scs_enum_values_enu_ord_un unique(enum_id,ordering)
+);
+
+-----------
+-- Views --
+-----------
+
+create or replace view scs_enums as
+select scs_enumerations.enum_id, scs_enumerations.name, 
+       scs_enum_values.val_id, scs_enum_values.value, scs_enum_values.active_p, 
+       scs_texts.text_id, 
+       scs_text_lang.lang_id, scs_text_lang.text,
+       scs_lang.language
+  from scs_enumerations, scs_enum_values, scs_texts, scs_lang, scs_text_lang
+ where scs_enumerations.enum_id = scs_enum_values.enum_id
+   and scs_enum_values.text_id = scs_texts.text_id
+   and scs_texts.text_id = scs_text_lang.text_id
+   and scs_text_lang.lang_id = scs_lang.lang_id;
+
+--------------
+-- TRIGGERS --
+--------------
+
+-- The trigger makes sure that the affiliated scs-texts are deleted
+-- when we delete an enumeration.
+create or replace trigger scs_enum_values_before_del_tr
+before delete on scs_enum_values
+for each row
+declare
+begin
+  scs_text.destroy(:old.text_id);
+end scs_enum_values_before_del_tr;
+/
+show errors;
