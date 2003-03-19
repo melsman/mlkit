1e321cf0115b2fc14e548fa9e58c09ac72385f97
Kennie Nybo Pontoppidan <knp@rehfeld.dk>
2003-03-19 15:45:46 +0000

    *** empty log message ***

---
+++ smlserver_demo/scs_lib/orasql/scs-approvals-packages-create.sql
@@ -0,0 +1,131 @@
+/* ======================================================================
+   package scs_approvals
+
+   functionality for approving rows
+
+   $Id$
+
+   History:
+   160303 Kennie Nybo Pontoppidan <kennie@it-c.dk> created package
+====================================================================== */ 
+create or replace package scs_approval
+is
+  /* ---------------------
+     procedure approve_row
+     ---------------------
+     inserts a row in scs_approvals with 
+       ON_WHAT_TABLE = table_name
+       ON_WHAT_ID = id
+       PARTY_ID = approved_by
+       DESICION = 't'
+       CREATED_ON = sysdate
+  */
+  procedure approve_row(
+    table_name  in varchar2,
+    id          in integer,
+    approved_by in integer
+  );
+
+  /* ---------------------
+     procedure decline_row
+     ---------------------
+     inserts a row in scs_approvals with 
+       ON_WHAT_TABLE = table_name
+       ON_WHAT_ID = id
+       PARTY_ID = approved_by
+       DESICION = 'f'
+       CREATED_ON = sysdate
+       NOTE_TEXT
+  */
+  procedure decline_row(
+    table_name  in varchar2,
+    id          in integer,
+    approved_by in integer,
+    note_text   in varchar
+  );
+  
+  /* ---------------------
+     procedure delete_rows
+     ---------------------
+     delete all rows in scs_approvals with 
+       ON_WHAT_TABLE = table_name
+       ON_WHAT_ID = id
+  */
+  procedure delete_rows(
+    table_name  in varchar2,
+    id          in integer
+  );
+
+
+end scs_approval;
+/ 
+show errors
+
+
+create or replace package body scs_approval
+is
+  procedure approve_row(
+    table_name  in varchar2,
+    id          in integer,
+    approved_by in integer
+  ) is
+  begin
+    insert into SCS_APPROVALS(
+      APPROVAL_ID,    
+      ON_WHAT_TABLE,  
+      ON_WHAT_ID,     
+      PARTY_ID,      
+      DECISION,       
+      CREATED_ON    
+    ) values(
+      scs.new_obj_id,
+      table_name,
+      id,
+      approved_by,
+      't',
+      sysdate
+    );
+  end approve_row;
+
+
+  procedure decline_row(
+    table_name  in varchar2,
+    id          in integer,
+    approved_by in integer,
+    note_text   in varchar
+  ) is
+  begin
+    insert into SCS_APPROVALS(
+      APPROVAL_ID,    
+      ON_WHAT_TABLE,  
+      ON_WHAT_ID,     
+      PARTY_ID,      
+      DECISION,       
+      CREATED_ON,
+      NOTE_TEXT
+    ) values(
+      scs.new_obj_id,
+      table_name,
+      id,
+      approved_by,
+      'f',
+      sysdate,
+      note_text
+    );
+  end decline_row;
+
+
+  procedure delete_rows(
+    table_name  in varchar2,
+    id          in integer
+  ) is
+  begin
+    delete scs_approvals
+     where ON_WHAT_TABLE = table_name
+       and ON_WHAT_ID = id;
+  end delete_rows;
+
+end scs_approval;
+/ 
+show errors
+
---
+++ smlserver_demo/scs_lib/orasql/scs-create-only-packages.sql
@@ -0,0 +1,36 @@
+-- $Id$
+
+
+------------------------------------
+-- LOAD THE ENTIRE SCS DATA MODEL --
+------------------------------------
+@scs-packages-create.sql
+@scs-math-packages-create.sql;
+@scs-test-packages-create.sql;
+
+@scs-logs-packages-create.sql;
+
+@scs-locales-packages-create.sql;
+
+@scs-enumerations-packages-create.sql;
+
+@scs-parties-packages-create.sql;
+
+@scs-persons-packages-create.sql;
+
+@scs-users-packages-create.sql;
+
+@scs-roles-packages-create.sql;
+@scs-groups-packages-create.sql;
+
+
+
+
+
+
+
+--@scs-dict-create.sql;
+
+select table_name from user_tables; -- debugging information
+set serveroutput on
+
