42e7358da4dbea6f7e50cea97287287fc5d54ed4
Carsten Varming <cvarming@cs.cmu.edu>
2005-11-16 23:54:43 +0000

    *** empty log message ***

---
+++ kit/src/SMLserver/apache/ulflat.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "parseul.h"
+
+int
+main(int argc, char **argv)
+{
+  int r;
+  struct parseCtx c;
+  if (argc < 2) return EXIT_FAILURE;
+  c.fileprefix = "/fileprefix/path/";
+  c.mapprefix = "/mapprefix/path/";
+  c.root = "/root//";
+  c.fpl = strlen(c.fileprefix);
+  c.mpl = strlen(c.mapprefix);
+  c.rl = strlen(c.root);
+  c.uoTable = NULL;
+  c.smlTable = NULL;
+  c.ulTable = NULL;
+  r = recurseParse(&c, argv[1]);
+  printf("recurseParse returned %d\n", r);
+  return EXIT_SUCCESS;
+}
